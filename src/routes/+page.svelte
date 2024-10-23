e78b55de27252ced6cc1315fdac42d6976fe247b
Ambro <ambroise.brocard@gmail.com>
2024-10-23 15:02:37 +0200

    ✨ setup table base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let table = [];
+
+    const resetTable = (size: number = 3) => {
+        table = [];
+
+        for (let rIndex = 0; rIndex < size; rIndex++) {
+            let row = [];
+            for (let cIndex = 0; cIndex < size; cIndex++) {
+                row.push(0);
+            }
+            table.push(row);
+        }
+    };
+
+    onMount(() => {
+        resetTable();
+    });
+</script>
+
+<div class="container p-2 flex flex-col items-center">
+    <h1>Welcome to OxO</h1>
+    <div class="flex gap-2 p-4">
+        {#each table as row}
+            <div class="flex flex-col gap-2">
+                {#each row as cell}
+                    <button class="bg-sky-500 hover:bg-sky-700 ...">{cell}</button>
+                {/each}
+            </div>
+        {/each}
+    </div>
+</div>
+
