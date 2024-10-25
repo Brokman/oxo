38859bd1bab81556b6db7177481e262ff6188f82
Ambro <ambroise.brocard@gmail.com>
2024-10-25 16:33:20 +0200

    💄 updated wining style

---
+++ src/lib/components/ConfettiExplosion.svelte
@@ -0,0 +1,31 @@
+<div>
+    <img
+    id="winning-confetti"
+    src="src/lib/images/wired-outline-1103-confetti-hover-wobble.gif"
+    alt="wining confetti"
+    height=1020
+    width=800
+    />
+</div>
+
+<style>
+
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  90% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+    #winning-confetti {
+        /* z-index: 1; */
+        /* transform: (-50%, -50%); */
+        /* animation: fadeOut 2s forwards; */
+        /* animation-fill-mode: forwards; */
+    }
+</style>
